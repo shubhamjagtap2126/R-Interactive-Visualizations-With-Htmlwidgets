1858fea418dd2a1cf9fb5c0f89873d83999bcaae
Shubham Jagtap <shubhamjagtap2126@gmail.com>
2021-08-16 06:34:08 +0530

    R Interactive Visualizations With Htmlwidgets = 06_04

---
+++ 06_04/interactive-table.R
@@ -0,0 +1,16 @@
+library("tidyverse")
+library("DT")
+library("stringr")
+
+transport_data <- read_csv("data/transport_data.csv")
+
+pretty_headers <- gsub("[.]", " ", colnames(transport_data)) %>%
+  str_to_title()
+
+transport_data %>%
+  datatable(
+    rownames = FALSE,
+    colnames = pretty_headers,
+    filter = list(position = "top"),
+    options = list(language = list(sSearch = "Filter:"))
+  )
